3aa1c358d5ba651c503057f1349eca8a26b10aa4
Marvin König <2164877+mkqavi@users.noreply.github.com>
2018-10-25 12:50:59 +0200

    Create a TCP server which sends stream messages

---
+++ index.js
@@ -0,0 +1,31 @@
+const StreamrClient = require('streamr-client');
+const net = require('net');
+
+const client = new StreamrClient({});
+
+let server = net.createServer((socket) => {
+  let sub;
+
+  socket.on('data', (data) => {
+    let stream = JSON.parse(data.toString('utf8')).stream;
+    sub = client.subscribe(
+      {
+        stream: stream,
+      },
+      (message, metadata) => {
+        socket.write(`${JSON.stringify(message)}\r\n`)
+      }
+    );
+  });
+
+  socket.on('error', (err) => {
+    console.error(`Fatal error: ${err}`);
+  });
+
+  socket.on('close', (had_error) => {
+    socket.destroy();
+    client.unsubscribe(sub);
+  });
+});
+
+server.listen(18292, '127.0.0.1');
